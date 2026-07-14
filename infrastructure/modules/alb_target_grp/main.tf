2105c90a98769e379eccbb56965982cf516b58e2
Pasindu <pasindumadushan722@gmail.com>
2026-07-15 00:06:17 +0530

    add health check configuration to ALB target group

--- infrastructure/modules/alb_target_grp/main.tf
+++ infrastructure/modules/alb_target_grp/main.tf
@@ -4,4 +4,19 @@ resource "aws_alb_target_group" "this" {
   protocol = var.alb-target-group-protocol
   vpc_id   = var.vpc-id
   target_type = "ip"
+
+  health_check {
+    enabled = true
+    path = "/health"
+    port = "traffic-port"
+    protocol = "HTTP"
+
+    healthy_threshold = 2
+    unhealthy_threshold = 3
+
+    timeout = 5
+    interval = 30
+
+    matcher = "200-399"
+  }
 }
